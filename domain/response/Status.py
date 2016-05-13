352c9c8b11d77253002e07dd757c47bea90023e6
Jiawei Wu <Jiawei.Wu@betfair.com>
2016-05-13 23:50:42 +0100

    Add new fields.

--- domain/response/Status.py
+++ domain/response/Status.py
@@ -9,7 +9,13 @@ class Status(Response):
             self._error_code = response["errorCode"]
         if "errorMessage" in response:
             self._error_message = response["errorMessage"]
+        if "connectionClosed" in response:
+            self._connection_closed = response["connectionClosed"]
 
     @property
     def status_code(self):
         return self._status_code
+
+    @property
+    def connection_closed(self):
+        return self._connection_closed
