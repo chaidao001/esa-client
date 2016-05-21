6e8287a8ab40abeaee52b8c6374688de2227c42e
Jiawei Wu <Jiawei.Wu@betfair.com>
2016-05-21 12:03:10 +0100

    Abstract method for updating img.

--- src/client/cache/cache.py
+++ src/client/cache/cache.py
@@ -10,19 +10,23 @@ class Cache:
 
     def on_receive(self, market_changes: list()):
         for market_change in market_changes:
-            market_id = market_change.id
             if market_change.img:
-                if market_change.market_def.status != MarketStatus.CLOSED:
-                    self._markets[market_id] = market_change
-                else:
-                    # remove if full img and already in cache
-                    if market_id in self._markets:
-                        logging.info("Market %s is closed.  Removing from cache" % market_id)
-                        self._markets.pop(market_id)
-                    else:
-                        logging.info("Market %s is closed.  Ignore" % market_id)
+                self._process_market_img(market_change)
             else:
                 self._update_market(market_change)
+
+    def _process_market_img(self, market_change: MarketChange):
+        market_id = market_change.id
+
+        if market_change.market_def.status != MarketStatus.CLOSED:
+            self._markets[market_id] = market_change
+        else:
+            # remove if full img and already in cache
+            if market_id in self._markets:
+                logging.info("Market %s is closed.  Removing from cache" % market_id)
+                self._markets.pop(market_id)
+            else:
+                logging.info("Market %s is closed.  Ignore" % market_id)
 
     def _update_market(self, market_change: MarketChange):
         market_id = market_change.id
