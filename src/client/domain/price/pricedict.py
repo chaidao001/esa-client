4f8396a214403fcf7e3af4bc99d61b5e6b875342
chaidao001 <chaidao0001@gmail.com>
2017-06-14 00:03:15 +0100

    Remove underscores and properties.

--- src/client/domain/price/pricedict.py
+++ src/client/domain/price/pricedict.py
@@ -1,18 +1,14 @@
 class PriceDict:
     def __init__(self, prices):
-        self._prices = {p[0]: p[1] for p in prices}
+        self.prices = {p[0]: p[1] for p in prices}
 
     def update(self, prices):
-        self._prices.update(prices.prices)
-        self._prices = {k: v for k, v in self._prices.items() if v > 0}
-
-    @property
-    def prices(self):
-        return self._prices
+        self.prices.update(prices.prices)
+        self.prices = {k: v for k, v in self.prices.items() if v > 0}
 
     def get_vol_for_price(self, price):
-        if price in self._prices:
-            return self._prices[price]
+        if price in self.prices:
+            return self.prices[price]
         else:
             return 0
 
